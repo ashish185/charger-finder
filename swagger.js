6fed48c9843f0bdb6986c0343fea6644a00adc0c
Ashish Singhal <singhalash185@gmail.com>
2026-08-07 21:47:34 +0530

    Fixed: Swagger config

--- swagger.js
+++ swagger.js
@@ -96,7 +96,7 @@ const options = {
       },
     },
   },
-  apis: ["./routes/*.js"], // Path to your route files
+  apis: ["./src/routes/*.js"], // Path to your route files
 };
 
 export default swaggerJsdoc(options);
