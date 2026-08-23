4b1569d18e89b55399e63302c70ac7e5a13ec0b8
Ashish Singhal <singhalash185@gmail.com>
2026-08-23 17:45:54 +0530

    Added: console for prod

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -59,10 +59,11 @@ authRouter.post("/otp/verify", async (req, res) => {
     );
 
     const isProd = process.env.NODE_ENV === "production";
+    console.log("************is Prod env", isProd);
     res.cookie("token", sessionToken, {
       httpOnly: true,
       secure: isProd,
-      sameSite: "none",
+      sameSite: isProd ? "none" : "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
